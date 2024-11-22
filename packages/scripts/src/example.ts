9f5c01f28aaa1da2a869804ceb3fae1394e2a38d
Aleksei Rozhnov <alekseyrozh@gmail.com>
2024-11-22 21:52:51 +0000

    initial setup

---
+++ packages/scripts/src/example.ts
@@ -0,0 +1,4 @@
+import { Resource } from "sst";
+import { Example } from "@borderless-passport-uploader/core/example";
+
+console.log(`${Example.hello()} Linked to ${Resource.MyBucket.name}.`);
---
+++ sst.config.ts
@@ -0,0 +1,19 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "borderless-passport-uploader",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+    };
+  },
+  async run() {
+    await import("./infra/storage");
+    const api = await import("./infra/api");
+
+    return {
+      api: api.myApi.url,
+    };
+  },
+});
